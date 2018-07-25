73dbb5705e4fe378eb8e06b7337872ef6267e3b8
Madeline Celis <madelinebcelis@gmail.com>
2018-07-25 21:08:28 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,28 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+
+  data.each do |seasonName, seasonData|
+      if seasonName == season
+        seasonData.each do |contestantInfoKey|
+          contestantInfoKey.each do |key_name, value|
+            if key_name = "status"
+              if value == 'Winner'
+                season
+                data[season].each do|obj|
+                  obj.each do |key, value|
+                    key
+                    return value.split(' ').slice(0, 1).join('')
+                  end
+                end #array of contestant objects
+              end
+            end # end of stats if loop
+          end # end of contestantInfoKey loop
+        end # end of seasonData loop
+      end # end of if loop
+  end # end of data loop
+end# end of function
+
 
 def get_contestant_name(data, occupation)
   # code here
